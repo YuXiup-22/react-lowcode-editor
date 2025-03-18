b7da4f2f9a5c1a2582749ae06752aa980572a08c
YuXiup-22 <tangtang5202019@163.com>
2025-03-18 22:09:20 +0800

    feat: 创建store维护json

--- src/editor/components/EditArea/index.tsx
+++ src/editor/components/EditArea/index.tsx
@@ -1,5 +1,17 @@
-export default function EditorArea(params:type) {
-    return <div>editorarea</div>
-    
-    
+import { useEffect } from "react";
+import { useComponentStore } from "../../stores/components";
+export default function EditorArea() {
+  const { components, addComponent } = useComponentStore();
+  useEffect(() => {
+    addComponent(
+      {
+        id: 2,
+        name: "测试",
+        props: {},
+        children: [],
+      },
+      1
+    );
+  }, []);
+  return <div><pre>{JSON.stringify(components, null, 2)}</pre></div>;
 }
--- src/editor/stores/components.tsx
+++ src/editor/stores/components.tsx
@@ -3,26 +3,102 @@
  */
 import { create } from "zustand";
 
-
-interface Component{
-    id:number,
-    name:string,
-    props:any,
-    children?:Component[],
-    parentId?:number
+interface Component {
+  id: number;
+  name: string;
+  props: any;
+  children?: Component[];
+  parentId?: number;
 }
 
 interface State {
-    components:Component[]
-
+  components: Component[];
 }
 
-interface Action{
-    addComponent:(component:Component,parentId?:number)=>void
-    deleteComponent:(componentId:number)=>void
-    updateComponentProps:(componentId:number,props:any)=>void
+interface Action {
+  addComponent: (component: Component, parentId?: number) => void;
+  deleteComponent: (componentId: number) => void;
+  updateComponentProps: (componentId: number, props: any) => void;
 }
 
-const useComponentStore = create<State&Action>(()=>(
-    
-))
+ const useComponentStore = create<State & Action>((set, get) => ({
+  components: [
+    {
+      id: 1,
+      name: "page",
+      props: {},
+      desc: "页面",
+    },
+  ],
+  addComponent: (component, parentId) =>
+    set((state) => {
+      if (parentId) {
+        const parentComponent = getComponentById(parentId, state.components);
+        if (parentComponent) {
+          if (parentComponent.children) {
+            parentComponent.children.push(component);
+          } else {
+            parentComponent.children = [component];
+          }
+          component.parentId = parentId;
+          return {
+            components: [...state.components],
+          };
+        }
+      }
+      return {
+        components: [...state.components, component],
+      };
+    }),
+  deleteComponent: (componentId: number) => {
+    if (!componentId) return null;
+    const component = getComponentById(componentId, get().components);
+    if (component?.parentId) {
+      const parentComponent = getComponentById(
+        component.parentId,
+        get().components
+      );
+      if (parentComponent) {
+        // +表示转换为数字类型
+        parentComponent.children = parentComponent.children?.filter(
+          (component) => component.id !== componentId
+        );
+        set({
+          components: [...get().components],
+        });
+      }
+    }
+  },
+  updateComponentProps: (componentId, props) =>
+    set((state) => {
+      const component = getComponentById(componentId, get().components);
+      if (component) {
+        component.props = { ...component.props, ...props };
+        return {
+          components: [...state.components],
+        };
+      }
+      return {
+        components: [...state.components],
+      };
+    }),
+}));
+
+function getComponentById(
+  id: number,
+  components: Component[]
+): Component | null {
+  if (!id) return null;
+  for (const component of components) {
+    if (component.id === id) return component;
+    if (component.children && component.children.length > 0) {
+      const result = getComponentById(id, component.children);
+      if (result !== null) return result;
+    }
+  }
+  return null;
+}
+export {
+  useComponentStore
+};
+export type { Component };
